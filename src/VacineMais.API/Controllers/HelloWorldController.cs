c2478b4947e03e7ef14de3d6dfd88532f4a346b0
lucas <lac.lucas96@gmail.com>
2024-07-19 20:13:04 -0300

    adiciona projetos

---
+++ src/VacineMais.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VacineMais.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloWorldController
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello World";
+    }
+}
---
+++ src/VacineMais.API/Program.cs
@@ -0,0 +1,36 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                      });
+});
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapControllers();
+
+app.Run();
