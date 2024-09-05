c304066732fb28ae5a3c69676dd3fac79d56ef4e
tabatasando <tabata.sa@hotmail.com>
2024-09-05 10:15:31 -0300

    tela home sem erro

--- src/VacineMais.Web/src/pages/Home/index.tsx
+++ src/VacineMais.Web/src/pages/Home/index.tsx
@@ -1,35 +1,7 @@
 export function Home() {
   return (
-  
-
   <>
       <p>Página Home</p>
-
-      <div class="off-screen-menu">
-<ul>
-<li><a href="/CarteiraVacinacao">Carteira Vacinação</a></li>
-<li><a href="/NovoMembro">Novo Membro</a></li>
-<li><a href="/NovoRegistro">Novo Registro</a></li>
-</ul>
-</div>
-
-<nav>
-<div class="ham-menu">
-<span></span>
-<span></span>
-<span></span>
-</div>
-</nav>
-  
     </>
   );
 }
-
-const hamMenu = document.querySelector(".ham-menu");
-
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
